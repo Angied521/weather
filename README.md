481567ecb49016751d47b7457ac9b767bcc6dd9a
Angie DePasquale <angied521@yahoo.com>
2019-12-16 14:20:55 -0500

    monday updates

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # weather
+
+<p>
+This is a weather app that gives the current condition for city selected
+
+Using AJAX to hook into the API to retrieve data.
+</p>
+
+</br>
+
+
+<p>The following condtions will be displayed:</>
+
+<ul>
+<li>City</li>
+<li>Date</li>
+<li>Temperature</li>
+<li>Humidity</li>
+<li>ect</li>
+
+</br>
+<p>
+
+
+
+Include a 5-Day Forecast below the current weather conditions. Each day for the 5-Day Forecast should display the following:
+</p>
+
+
--- main.js
+++ main.js
@@ -1,26 +1,43 @@
-$(document).ready(function(){
-    $('#submitWeather').click(function(){
+$(document).ready(function () {
+    // to request data from openweather.org
+    $('#submitWeather').click(function () {
 
-            var city = $("#city").val();
-            if (city != ''){
+        var city = $("#city").val();
+        if (city != '') {
 
-                $.ajax({
+            $.ajax({
 
-                    url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city +"&units=metric" + "de0beaf5a28fc57eed081e333502c625",
-                    type: "GET",
-                    dataType: "jsonp",
-                    success: function(data){
-                        console.log(data);
-                    }
+                url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=de0beaf5a28fc57eed081e333502c625",
+                type: "GET",
+                dataType: "jsonp",
+                success: function (data) {
+                    //console.log(data);
+                    var widget = show(data);
 
-                   
-                });
+                    $("#show").html(widget);
 
-            }else{
-                $("#error").html('Field cannot be empty');
-            }
+                    $("#city").val();
+                }
+            });
 
+        } else {
+            $("#error").html('Field cannot be empty');
+        }
     });
 
-
 });
+
+//return data for weather temp, humidity, wind, uv index
+function show(data) {
+    return "<h2>Current Weather for " + data.name +"," + data.sys.country +"</h2>" +
+    
+           "<h3><strong>Weather</strong>: " + data.weather[0].main + "</h3>" +
+           "<h3><strong>Description</strong>: " + data.weather[0].description + "</h3>" +
+           "<h3><strong>Temperature</strong>: " + data.main.temp + "</h3>" +
+           "<h3><strong>Pressure</strong>: " + data.main.pressure + "</h3>" +
+           "<h3><strong>Humidity</strong>: " + data.main.humidity + "</h3>" +
+           "<h3><strong>Min Temperature</strong>: " + data.main.temp_min + "</h3>" +
+           "<h3><strong>Max Temperature</strong>: " + data.main.tem_max + "</h3>" +
+           "<h3><strong>Wind Direction</strong>: " + data.wind.speed +"</h3>";
+
+}
